bb0c940ed136299fd5647ce04fa6d52db410bb18
ethanmiles <percivalstr@163.com>
2019-04-01 23:13:23 +0800

    update

--- run.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-echo " compiling... "
-
-g++ -std=c++11 -o debug/bnn src/main.cpp 
-
-exit 0
--- src/xml_parser.hpp
+++ src/xml_parser.hpp
@@ -1,117 +1,168 @@
 //
-//  xml_parser.hpp
-//  bayesian_network_for_nlp
+//  xmlParser.h
+//  bayesian-network-for-nlp
 //
-//  Created by Ethan Miles on 2019/3/29.
+//  Created by Ethan Miles on 2019/3/31.
 //  Copyright © 2019 Ethan Miles. All rights reserved.
 //
 
-#ifndef xml_parser_h
-#define xml_parser_h
+#ifndef xmlParser_h
+#define xmlParser_h
 
 #include <fstream>
 #include <iostream>
-#include <set>
 #include <string>
+#include <algorithm>
+#include <streambuf>
+#include <regex>
+#include "log.h"
+
+//class node {
+//    std::string tag;
+//    std::string::const_iterator beg;
+//    std::string::const_iterator end;
+//    std::vector<node> children;
+//
+//public:
+//    node() = delete;
+//    node(std::string tag):tag(std::move(tag)){};
+//    node(std::string tag,std::string::const_iterator beg,std::string::const_iterator end):tag(std::move(tag)),beg(beg),end(end) {};
+//    ~node() = default;
+//};
 
 class xmlParser {
- private:
-  std::string inputPath;
-  std::string outputPath;
-  std::set<std::string> filterTable;
-
-  std::ifstream originXML;
-
- public:
-  xmlParser() = delete;
-  xmlParser(std::string input, std::string defaultOutput)
-      : inputPath(std::move(input)), outputPath(std::move(defaultOutput)) {
-    originXML.open(inputPath);
-    if (!originXML.is_open()) {
-      std::cout << "failed to open " << inputPath << '\n';
+private:
+    std::string defaultOutputPath;
+public:
+    xmlParser() = delete;
+    xmlParser(std::string defaultOutput):defaultOutputPath(std::move(defaultOutput)) {};
+    ~xmlParser() {
+    };
+    
+    //    std::shared_ptr<node> generateDOM (std::string from, std::string rootTag) {
+    //        std::ifstream originXML{from};
+    //        if(!originXML.is_open()){
+    //            log("!originXML.is_open()","fail to read");
+    //            return nullptr;
+    //        }
+    //        std::string str{(std::istreambuf_iterator<char>(originXML)), std::istreambuf_iterator<char>()};
+    //
+    //        node root {"_root_"};
+    //
+    //        const std::string tagBegin = std::string("<") + rootTag + ">";
+    //        const std::string tagEnd = std::string("</") + rootTag + ">";
+    //
+    //        return std::make_shared<node>(root);
+    //    }
+    
+    void enwikiProcess(std::string from, std::string to) {
+        struct {
+            std::string splitTag = "page";
+            std::int64_t splitLines = 1000000;// 1,000,000
+        } config;
+        
+        splitToFiles(from, config.splitTag, config.splitLines, to);
+        
     }
-  };
-  ~xmlParser() = default;
-
-  // [describe] split xml using default output path
-  // [parameter] tag : "xxx" ( "<xxx>" is invalid)
-  // [bug] may generate an extra blank file
-  void splitToFiles(std::string tag, int64_t maxProcessLines) {
-    splitToFiles(tag, maxProcessLines, outputPath);
-  }
-
-  // [describe] split xml using assign path
-  // [parameter] tag : "xxx" ( "<xxx>" is invalid)
-  // [bug] may generate an extra blank file
-  void splitToFiles(std::string tag, int64_t maxProcessLines,
-                    std::string outputDir) {
-    std::string tagBegin = "<" + tag + ">";
-    std::string tagEnd = "</" + tag + ">";
-    int64_t currentLines = 0;
-    std::string line;
-    bool reading = false;
-    bool empty = true;
-    int dirIndex = 1;
-    std::string validOutput;
-    std::ofstream ostream{outputDir + std::to_string(dirIndex)};
-    size_t endPos = 0;
-    size_t beginPos = 0;
-
-    for (; currentLines <= maxProcessLines;) {
-      if (empty == true) {
-        std::getline(originXML, line);
-        ++currentLines;
-      } else {
-        line = line.substr(endPos + tagEnd.size(), -1);
-        ++dirIndex;
-        ostream.close();
-        ostream.open(outputDir + std::to_string(dirIndex));
-      }
-
-      if (reading == true) {
-        endPos = line.find(tagEnd, 0);
-        if (endPos != -1) {
-          validOutput = line.substr(0, endPos);  // "+++++</T>?????"
-          reading = false;
-          empty = false;
-        } else {
-          validOutput = line;  // "+++++++++++"
-          empty = true;
-          reading = true;
-        }
-      } else {  // (reading == false)
-        beginPos = line.find(tagBegin, 0);
-        if (beginPos != -1) {
-          endPos = line.find(tagEnd, 0);
-          if (endPos != -1) {
-            validOutput =
-                line.substr(beginPos + tagBegin.size(),
-                            endPos - beginPos -
-                                tagBegin.size());  //"-----<T>+++++++</T>?????"
-            reading = false;
-            empty = false;
-          } else {
-            validOutput =
-                line.substr(beginPos + tagBegin.size(), -1);  //"---<T>+++++++"
-            empty = true;
-            reading = true;
-          }
-        } else {
-          validOutput = "";  //"---------"
-          empty = true;
-        }
-      }
-      ostream << validOutput;
-
-      if (originXML.eof()) {
-        break;
-      }
-    }  // end for
-
-    if (ostream.is_open()) {
-      ostream.close();
+    
+    // check the intergity of the xml document.
+    bool xmlIntegrity (std::istream& i){
+        return false;
     }
-  };
+    // [describe] split xml using default output path
+    // [parameter] tag : "html" ( "<html>" is invalid),
+    //             just split outside tag, doesn't process nested tag with same name.
+    // [bug] may generate an extra blank file
+    void splitToFiles(std::string from, std::string tag, int64_t maxProcessLines) {
+        splitToFiles(from, tag, maxProcessLines, defaultOutputPath);
+    }
+    
+    // [describe] split xml using assign path
+    // [parameter] tag : "xxx" ( "<xxx>" is invalid)
+    //             just split outside tag, doesn't process nested tag with same name.
+    // [bug] may generate an extra blank file
+    void splitToFiles(std::string from, std::string tag, int64_t maxProcessLines,
+                      std::string outputDir) {
+        std::ifstream originXML;
+        originXML.open(from);
+        if(!originXML.is_open()){
+            log("!originXML.is_open()","fail to read");
+            return ;
+        }
+        std::string tagBegin = std::string("<") + tag + ">";
+        std::string tagEnd = std::string("</") + tag + ">";
+        if(tag.compare("html") == 0){
+            tagBegin = std::string("<") + tag + " xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\" dir=\"ltr\">";
+        }
+        int64_t currentLines = 0;
+        std::string line;
+        bool reading = false;
+        bool empty = true;
+        int dirIndex = 1;
+        std::string validOutput;
+        std::ofstream ostream{outputDir + std::to_string(dirIndex)};
+        size_t endPos = 0;
+        size_t beginPos = 0;
+        
+        for (; currentLines <= maxProcessLines;) {
+            if (empty == true) {
+                std::getline(originXML, line);
+                ++currentLines;
+            } else {
+                line = line.substr(endPos + tagEnd.size(), -1);
+                ++dirIndex;
+                ostream.close();
+                log("finished write file "+std::to_string(dirIndex),"");
+                ostream.open(outputDir + std::to_string(dirIndex));
+            }
+            if (reading == true) {
+                endPos = line.find(tagEnd, 0);
+                if (endPos != -1) {
+                    validOutput = line.substr(0, endPos);  // "+++++</T>?????"
+                    reading = false;
+                    empty = false;
+                } else {
+                    validOutput = line;  // "+++++++++++"
+                    empty = true;
+                    reading = true;
+                }
+            } else {  // (reading == false)
+                beginPos = line.find(tagBegin, 0);
+                if (beginPos != -1) {
+                    endPos = line.find(tagEnd, 0);
+                    if (endPos != -1) {
+                        validOutput =
+                        line.substr(beginPos + tagBegin.size(),
+                                    endPos - beginPos -
+                                    tagBegin.size());  //"-----<T>+++++++</T>?????"
+                        reading = false;
+                        empty = false;
+                    } else {
+                        validOutput =
+                        line.substr(beginPos + tagBegin.size(), -1);  //"---<T>+++++++"
+                        empty = true;
+                        reading = true;
+                    }
+                } else {
+                    validOutput = "";  //"---------"
+                    empty = true;
+                }
+            }
+            ostream << validOutput;
+            
+            if (originXML.eof()) {
+                break;
+            }
+        }  // end for
+        
+        // postprocess
+        if (ostream.is_open()) {
+            ostream.close();
+        }
+        if(originXML.is_open()){
+            originXML.close();
+        }
+    };
 };
 
-#endif /* xml_parser_h */
+#endif /* xmlParser_h */
