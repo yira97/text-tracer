684ed9f962ce9e75bcefd2da290d831a7d3a7271
ethanmiles <percivalstr@163.com>
2019-04-03 16:00:32 +0800

    build graph

--- README.md
+++ README.md
@@ -1,19 +1,38 @@
 # Bayesian Network For NLP
+there is two version of this project
+1. c++11(decrepit)
+2. python3
+## install
+### 1. get the latest version of wiki dumpfile
 
-## 1. corpus processing
+    $ curl https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2  
+    $ 7z e enwiki-latest-pages-articles.xml.bz2
+### 2. parse the origin xml using [wikiextractor](https://github.com/attardi/wikiextractor)
+    
+    $ git clone https://github.com/attardi/wikiextractor && cd wikiextractor
+    $ python3 setup.py install
+    $ python3 --html -s WikiExtractor.py
+### 3. build graph
+    $ cd .. && git clone https://github.com/ethanmiles/Bayesian-Network-for-NLP && cd Bayesian-Network-for-NLP
+    $ python3 xmlParser.py
+
+### 4. inference test
+    $ python3 xmlParser.py -t "Heavy metal music"
+## dev schedule
+### 1. corpus processing
 xml_parser.hpp contains a series of xml processing tools which can basically cover various requirement of student or scholar.
 
 Method Preview:
 * [x] split tags 
 ---
-## 2. bayesian graph construction
+### 2. bayesian graph construction
 * [x] edges
 * [x] nodes
 * [ ] probabilistic table
 * [ ] bayesian network inference
 ---
-## 3. advance application
-* [ ] building graph from structured documents
+### 3. advance application
+* [x ] building graph from structured documents
 ---
 ## 4. test
 * [x] graph_test
--- src/xmlParser.py
+++ src/xmlParser.py
@@ -1,19 +1,85 @@
 import re
 import os
-# extraRule need dict of string type re
 
 nameIndex = 1
 
 
-def wikiStanderdize(inputDir, outputDir):
-    rule = ["<doc>.*?<h1>.*?</h1>\n.*?may refer to[\s\S]*?</doc>", "<h2>See also</h2>", "<h2>References</h2>",
-            "<h3>Specific</h3>", "<h3>General</h3>", "<h2>External links</h2>"]
+class vertex:
+    def __init__(self, h, txt):
+        self.h = h
+        self.txt = txt
+        self.children = []
+
+
+class graph:
+    def __init__(self, inputDir, deep=4):
+        self.roots = []
+        self.terms = []
+
+        for f in allfilepaths(inputDir):
+            with open(f, "r", encoding="utf-8") as f_read:
+                docStr = f_read.read()
+                self.roots.append(getDocNodes(docStr, deep))
+
+        self.terms = getAllTerminologys(inputDir, deep)
+
+
+def getDocNodes(txt, maxDeep=4, h=1):  # do not pass h paramerter.
+    if len(txt) == 0:
+        return
+    stri = r"<h"+str(h)+r">.*?</h"+str(h) + \
+        r">([\s\S]*?)(?=<h" + str(h)+r">(.*?)</h"+str(h)+r")"
+    inners = re.findall(stri, txt)
+
+    if len(inners) > 0:
+        strLast = r"<h"+str(h)+r">" + \
+            inners[-1][-1]+r"</h"+str(h)+r">([\s\S]*)"
+    else:
+        strLast = r"<h"+str(h)+r">.*?</h"+str(h)+r">([\s\S]*)"
+    last = re.findall(strLast, txt)
+    if len(last) > 0:
+        inners.append((last[0], ""))
+    children = []
+    if len(inners) != 0:
+
+        for i in inners:
+            if (h <= maxDeep):
+                children.append(getDocNodes(i[0], maxDeep, h+1))
+    v = vertex(h - 1, txt)
+    v.children = children
+    return v
+
+
+def allfilepaths(inputDir, containHidden=False):
+    inputFileNames = []
     for root, _, files in os.walk(inputDir):
         for afile in files:
-            if '.' not in afile:
+            if '.' not in afile or containHidden == True:
                 inputFileName = root+'/'+afile
-                print("start process : ", inputFileName)
-                standerdize(inputFileName, outputDir, rule)
+                inputFileNames.append(inputFileName)
+    return inputFileNames
+
+
+def getAllTerminologys(inputDir, deep):
+    terms = []
+    stri = ""
+    for i in range(1, deep+1):
+        stri += str(i)
+    for f in allfilepaths(inputDir):
+        with open(f, "r", encoding="utf-8") as f_read:
+            txt = f_read.read()
+            for t in re.findall(r"<h["+stri+r"]>(.*?)</h\d>", txt):
+                terms.append(t)
+    return set(terms)
+
+
+def wikiStanderdize(inputDir, outputDir):
+
+    rule = [r"<doc>.*?<h1>.*?</h1>\n.*?may refer to[\s\S]*?</doc>", r"<h2>See also</h2>", r"<h2>References</h2>",
+            r"<h3>Specific</h3>", r"<h3>General</h3>", r"<h2>External links</h2>"]
+    for f in allfilepaths(inputDir):
+        print("start process : ", f)
+        standerdize(f, outputDir, rule)
 
 
 def standerdize(inputFilePath, outputDir, extraRules, splitTag="doc"):
@@ -24,12 +90,12 @@ def standerdize(inputFilePath, outputDir, extraRules, splitTag="doc"):
 
     txt = re.sub("<doc.*>", "<doc>", txt)
     txt = re.sub("<li>.*</li>", "", txt)
-    txt = re.sub("<ul>[\s\S]*?</ul>", "", txt)
+    txt = re.sub(r"<ul>[\s\S]*?</ul>", "", txt)
     txt = re.sub("</ul>", "", txt)
     txt = re.sub("&lt;/a&gt;", "", txt)
     txt = re.sub("&lt;a href=.*?&gt;", "", txt)
-    txt = re.sub("\[.*\]", " ", txt)
-    txt = re.sub("\(.*\)", "", txt)
+    txt = re.sub(r"\[.*\]", " ", txt)
+    txt = re.sub(r"\(.*\)", "", txt)
     txt = re.sub("&amp;", " ", txt)
     txt = re.sub("&nbsp;", " ", txt)
     txt = re.sub("&lg;", " ", txt)
@@ -41,11 +107,11 @@ def standerdize(inputFilePath, outputDir, extraRules, splitTag="doc"):
     txt = re.sub("[\n]+", "\n", txt)
     txt = re.sub('"', " ", txt)  # repace  " character with space
     txt = re.sub(",", " ", txt)  # replace comma character with space
-    txt = re.sub("\.", " ", txt)  # replace point character with space
+    txt = re.sub(r"\.", " ", txt)  # replace point character with space
     txt = re.sub(";", " ", txt)  # replace ; character with space
     txt = re.sub(":", " ", txt)  # replace ; character with space
 
-    splitRE = "<"+splitTag+">[\s\S]*?</"+splitTag+">"
+    splitRE = "<"+splitTag+r">[\s\S]*?</"+splitTag+">"
     txts = re.findall(splitRE, txt)
     if txts.count == 0:
         print("split fail")
@@ -57,7 +123,14 @@ def standerdize(inputFilePath, outputDir, extraRules, splitTag="doc"):
             f_write.write(t)
 
 
-if __name__ == "__main__":
-    inputFilePath = "/Users/ethan/Development/wikiextractor/text2/"
+def init():
+    #inputFilePath = "/Users/ethan/Development/wikiextractor/text2/"
     outputFilePath = "/Users/ethan/Development/temp/s/"
-    wikiStanderdize(inputFilePath, outputFilePath)
+
+    # wikiStanderdize(inputFilePath, outputFilePath)
+    t = graph(outputFilePath, 3)
+    print(t)
+
+
+if __name__ == "__main__":
+    init()
